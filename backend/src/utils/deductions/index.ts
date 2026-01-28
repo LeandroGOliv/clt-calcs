3cb6e8ab55d30ab6e53c9445828348c47016f13d
Leandro <leandro.golub123@gmail.com>
2026-01-27 22:53:37 -0300

    wip: adiciona calculo do IRRF

--- backend/src/utils/deductions/index.ts
+++ backend/src/utils/deductions/index.ts
@@ -4,7 +4,7 @@ import calcIRRF from "./irrf";
 export function applyCLTDeductions(gross: number) {
   const inss = calcINSS(gross);
   const irrfBase = gross - inss;
-  const irrf = calcIRRF(irrfBase, "ferias");
+  const irrf = calcIRRF(irrfBase);
   const net = gross - irrf - inss;
 
   return { inss, irrf, net };
--- backend/src/utils/deductions/irrf.ts
+++ backend/src/utils/deductions/irrf.ts
@@ -1,1 +1,20 @@
-export default function calcIRRF(gross: number, type: string) {}
+export default function calcIRRF(base: number): number {
+  let irrf = 0;
+
+  if (base <= 2428.8) {
+    irrf = 0;
+  } else if (base <= 2826.65) {
+    irrf = base * 0.075 - 182.16;
+  } else if (base <= 3751.05) {
+    irrf = base * 0.15 - 394.16;
+  } else if (base <= 4664.68) {
+    irrf = base * 0.225 - 675.49;
+  } else {
+    irrf = base * 0.275 - 908.73;
+  }
+
+  // fonte:  https://www.gov.br/secom/pt-br/acompanhe-a-secom/noticias/2026/01/nova-tabela-do-ir-veja-faixas-e-aliquotas-e-saiba-mais-sobre-medida-que-isenta-o-pagamento-para-quem-ganha-ate-r-5-mil
+
+  // FALTAM REDUÇÕES, entender como funcionam(ta no chat com gpt) e aplicar
+  return Math.max(Number(irrf.toFixed(2)), 0);
+}
