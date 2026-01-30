b5e0e564695fd0824c5370d3085ea00000be0b21
Leandro <leandro.golub123@gmail.com>
2026-01-29 21:49:44 -0300

    feat: adiciona reduções no cálculo do IRRF

--- backend/src/utils/deductions/irrf.ts
+++ backend/src/utils/deductions/irrf.ts
@@ -1,6 +1,7 @@
 export default function calcIRRF(base: number): number {
   let irrf = 0;
 
+  // IRRF
   if (base <= 2428.8) {
     irrf = 0;
   } else if (base <= 2826.65) {
@@ -13,8 +14,15 @@ export default function calcIRRF(base: number): number {
     irrf = base * 0.275 - 908.73;
   }
 
+  // Reduções
+  if (base <= 5000) {
+    irrf = 0;
+  } else if (base <= 7350) {
+    const reduction = 978.62 - 0.133145 * base;
+    irrf = irrf - reduction;
+  }
+
   // fonte:  https://www.gov.br/secom/pt-br/acompanhe-a-secom/noticias/2026/01/nova-tabela-do-ir-veja-faixas-e-aliquotas-e-saiba-mais-sobre-medida-que-isenta-o-pagamento-para-quem-ganha-ate-r-5-mil
 
-  // FALTAM REDUÇÕES, entender como funcionam(ta no chat com gpt) e aplicar
   return Math.max(Number(irrf.toFixed(2)), 0);
 }
