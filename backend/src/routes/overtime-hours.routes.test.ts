37630e62e196de73bbaaaaed5185f7cd14155ec1
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2026-02-17 17:27:45 -0300

    wip: teste route horas extras

--- backend/src/routes/overtime-hours.routes.test.ts
+++ backend/src/routes/overtime-hours.routes.test.ts
@@ -1,1 +1,25 @@
-// wip
+import { OvertimeHoursInput } from "@/schemas/overtime-hours.schema";
+
+const body = <OvertimeHoursInput>{
+  grossSalary: 2700,
+  monthlyWorkHours: 176,
+  overtimeHours: {
+    daily: 0,
+    night: 0,
+    holiday: 15,
+    holidayAndNight: 0,
+  },
+};
+
+describe("Faz calculo de horas extras", () => {
+  it("POST to /overtime-hours should return 200", async () => {
+    const response = await fetch("http://localhost:3333/overtime-hours", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    expect(response.status).toBe(200);
+  });
+});
