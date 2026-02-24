cd51111228e95ba48812a4bc692e20718eb89e82
Leandro <leandro.golub123@gmail.com>
2026-02-23 23:22:12 -0300

    feat: adiciona testes das services de décimo terceiro salário e de férias

--- backend/src/services/vacation.service.test.ts
+++ backend/src/services/vacation.service.test.ts
@@ -1,4 +1,5 @@
 import { applyCLTDeductions } from "@/utils/deductions";
+import { vacationCalc } from "./vacation.service";
 
 jest.mock("@/utils/deductions");
 
@@ -10,7 +11,52 @@ describe("vacationCalc", () => {
       .mockReturnValue({ inss: 100, irrf: 50, net: 850 });
   });
 
-  it("should calculate vacations pay correctly", () => {});
+  it("should calculate vacations pay correctly", () => {
+    const input = {
+      grossSalary: 5000,
+      sellVacationDays: true,
+      vacationDays: 30,
+      averageExtraEarnings: 200,
+    };
 
-  it("should apply CLT deductions on total gross pay", () => {});
+    const dailyRate =
+      (input.grossSalary + input.averageExtraEarnings) / input.vacationDays;
+
+    const expectedVacationPay = dailyRate * input.vacationDays;
+    const expectedVacationBonus = expectedVacationPay / 3;
+    const expectedSellValue = dailyRate * 10;
+    const expectedSellBonus = expectedSellValue / 3;
+    const expectedTotalVacationGrossPay =
+      expectedVacationPay +
+      expectedVacationBonus +
+      expectedSellValue +
+      expectedSellBonus;
+
+    const result = vacationCalc(input);
+
+    expect(result.vacationPay).toBeCloseTo(expectedVacationPay);
+    expect(result.vacationBonus).toBeCloseTo(expectedVacationBonus);
+    expect(result.vacationSellValue).toBeCloseTo(expectedSellValue);
+    expect(result.vacationSellBonus).toBeCloseTo(expectedSellBonus);
+    expect(result.totalVacationGrossPay).toBeCloseTo(
+      expectedTotalVacationGrossPay,
+    );
+  });
+
+  it("should apply CLT deductions on total gross pay", () => {
+    const input = {
+      grossSalary: 5000,
+      sellVacationDays: false,
+      vacationDays: 30,
+      averageExtraEarnings: 0,
+    };
+    vacationCalc(input);
+
+    const expectedTotalVacationGrossPay = 5000 + 5000 / 3;
+
+    expect(jest.mocked(applyCLTDeductions)).toHaveBeenCalledTimes(1);
+    expect(jest.mocked(applyCLTDeductions)).toHaveBeenCalledWith(
+      expect.closeTo(expectedTotalVacationGrossPay, 5),
+    );
+  });
 });
